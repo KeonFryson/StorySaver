b1e8047915612f6276edeb0f48586366cd8341c6
Keon Fryson <frysonk112115@gmail.com>
2025-09-22 15:41:22 -0400

    Add CORS header and new host permission
    Added 'Access-Control-Allow-Origin' header to JSON responses in index.js to enable cross-origin requests. Updated manifest.json to include 'https://storysaver.k-m-fryson112115.workers.dev/*' in host permissions for the Chrome extension.

--- src/index.js
+++ src/index.js
@@ -243,5 +243,6 @@ function json(data, status = 200) {
 	return new Response(JSON.stringify(data), {
 		status,
 		headers: { "Content-Type": "application/json" },
+		"Access-Control-Allow-Origin": "*",
 	});
 }
