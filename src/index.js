6d0baf34d167e04b2ef2f66a28416ee10d7be187
Keon Fryson <frysonk112115@gmail.com>
2025-09-22 01:17:11 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,18 +7,17 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
-
+import WebHtml from './Webpage.html';
 export default {
 	async fetch(request, env) {
 		const url = new URL(request.url);
 
 		// Serve HTML page
 		if (url.pathname === '/') {
-			return new Response(shopHtml, {
+			return new Response(WebHtml, {
 				headers: { 'Content-Type': 'text/html; charset=UTF-8' },
 			});
 		}
-
 
 	},
 };
